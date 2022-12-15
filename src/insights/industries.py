f1d771b1d9ab9f035905e063da8bcc573240e832
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-12-15 02:19:04 -0300

    feature: get unique industries req 3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,23 +1,9 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
-
+    return set([job["industry"] for job in read(path) if job["industry"] != ""])
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     """Filters a list of jobs by industry
