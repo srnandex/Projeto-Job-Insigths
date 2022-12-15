611a78979258f90bda2224055c48d0ce6d5912a6
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-12-15 04:57:07 -0300

    req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,20 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert mock[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+    assert mock[1] == {
+        "title": "Motorista",
+        "salary": "3000",
+        "type": "full time",
+    }
+    assert mock[2] == {
+        "title": "Analista de Software",
+        "salary": "4000",
+        "type": "full time",
+    }
